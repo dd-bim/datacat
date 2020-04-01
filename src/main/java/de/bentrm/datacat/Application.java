619d7fc710b587224e8e20bd5426784059d2975c
Benjamin Thurm <bentrm@posteo.de>
2020-04-01 11:33:05 +0200

    feat: Activate custom repository base class

--- src/main/java/de/bentrm/datacat/Application.java
+++ src/main/java/de/bentrm/datacat/Application.java
@@ -1,12 +1,16 @@
 package de.bentrm.datacat;
 
+import de.bentrm.datacat.repository.impl.GraphEntityRepositoryFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
-@EnableNeo4jRepositories(basePackages = {"de.bentrm.datacat.repository"})
+@EnableNeo4jRepositories(
+		basePackages = {"de.bentrm.datacat.repository"},
+		repositoryFactoryBeanClass = GraphEntityRepositoryFactoryBean.class
+)
 @SpringBootApplication
 public class Application {
 
