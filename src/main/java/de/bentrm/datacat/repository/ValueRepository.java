f1d69791fd35c223f66fb385ea85a2acee3bf853
Benjamin Thurm <bentrm@posteo.de>
2020-04-14 10:47:54 +0200

    feat: Add XtdValue repository

---
+++ src/main/java/de/bentrm/datacat/repository/ValueRepository.java
@@ -0,0 +1,8 @@
+package de.bentrm.datacat.repository;
+
+import de.bentrm.datacat.domain.XtdValue;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ValueRepository extends GraphEntityRepository<XtdValue, String> {
+}
