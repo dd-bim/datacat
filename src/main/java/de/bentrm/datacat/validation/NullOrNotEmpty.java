6b884a7412053dc3ecd26d5668ddc1dc1f9df283
Benjamin Thurm <bentrm@posteo.de>
2020-04-14 10:45:13 +0200

    feat: Add custom null or not empty validator

---
+++ src/main/java/de/bentrm/datacat/validation/NullOrNotEmpty.java
@@ -0,0 +1,27 @@
+package de.bentrm.datacat.validation;
+
+import org.hibernate.validator.constraints.CompositionType;
+import org.hibernate.validator.constraints.ConstraintComposition;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import javax.validation.ReportAsSingleViolation;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Null;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@ConstraintComposition(CompositionType.OR)
+@Null
+@NotBlank
+@ReportAsSingleViolation
+@Target({ ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = { })
+public @interface NullOrNotEmpty {
+    String message() default "{org.hibernate.validator.constraints.NullOrNotBlank.message}";
+    Class<?>[] groups() default { };
+    Class<? extends Payload>[] payload() default { };
+}
