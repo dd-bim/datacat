f627624faa067fe5418738d26b6753451eeff4d4
Benjamin Thurm <bentrm@posteo.de>
2020-04-14 11:14:42 +0200

    fix: Remove unnecessary properties of XtdMeasureWithUnit

--- src/main/java/de/bentrm/datacat/domain/XtdMeasureWithUnit.java
+++ src/main/java/de/bentrm/datacat/domain/XtdMeasureWithUnit.java
@@ -2,9 +2,6 @@ package de.bentrm.datacat.domain;
 
 import org.neo4j.ogm.annotation.NodeEntity;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @NodeEntity(label = XtdMeasureWithUnit.LABEL)
 public class XtdMeasureWithUnit extends XtdObject {
 
@@ -12,25 +9,4 @@ public class XtdMeasureWithUnit extends XtdObject {
     public static final String TITLE_PLURAL = "Measures";
     public static final String LABEL = PREFIX + "MeasureWithUnit";
 
-    private XtdUnit unitComponent;
-
-    private List<XtdValue> valueDomain = new ArrayList<>();
-
-    public XtdUnit getUnitComponent() {
-        return unitComponent;
-    }
-
-    public XtdMeasureWithUnit setUnitComponent(XtdUnit unitComponent) {
-        this.unitComponent = unitComponent;
-        return this;
-    }
-
-    public List<XtdValue> getValueDomain() {
-        return valueDomain;
-    }
-
-    public XtdMeasureWithUnit setValueDomain(List<XtdValue> valueDomain) {
-        this.valueDomain = valueDomain;
-        return this;
-    }
 }
