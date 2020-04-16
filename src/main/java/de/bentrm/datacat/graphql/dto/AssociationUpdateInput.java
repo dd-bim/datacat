abe67ed8f4604e99a61606b0db4b1c9a0959f14c
Benjamin Thurm <bentrm@posteo.de>
2020-04-16 15:05:06 +0200

    fix: Subclass dto base class

--- src/main/java/de/bentrm/datacat/graphql/dto/AssociationUpdateInput.java
+++ src/main/java/de/bentrm/datacat/graphql/dto/AssociationUpdateInput.java
@@ -2,52 +2,17 @@ package de.bentrm.datacat.graphql.dto;
 
 import de.bentrm.datacat.validation.IdConstraint;
 
-import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AssociationUpdateInput {
-
-    @NotBlank @IdConstraint
-    private String id;
-
-    @NotNull
-    private String versionId;
-
-    @NotNull
-    private String versionDate;
-
-    private @NotEmpty List<@Valid @NotNull TextInput> names = new ArrayList<>();
-
-    private List<@Valid @NotNull TextInput> descriptions = new ArrayList<>();
+public class AssociationUpdateInput extends RootUpdateInput {
 
     @NotBlank @IdConstraint
     private String relatingThing;
 
     private @NotNull List<@NotBlank @IdConstraint String> relatedThings = new ArrayList<>();
-
-    public String getId() {
-        return id;
-    }
-
-    public String getVersionId() {
-        return versionId;
-    }
-
-    public String getVersionDate() {
-        return versionDate;
-    }
-
-    public List<TextInput> getNames() {
-        return names;
-    }
-
-    public List<TextInput> getDescriptions() {
-        return descriptions;
-    }
 
     public String getRelatingThing() {
         return relatingThing;
