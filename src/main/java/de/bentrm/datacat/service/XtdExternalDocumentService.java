3a34dd625e54312f4071ec7094014b4013f7dd62
Benjamin Thurm <bentrm@posteo.de>
2020-04-16 15:04:30 +0200

    fix: Remove unused services

--- src/main/java/de/bentrm/datacat/service/XtdExternalDocumentService.java
+++
@@ -1,5 +0,0 @@
-package de.bentrm.datacat.service;
-
-import de.bentrm.datacat.domain.XtdExternalDocument;
-
-public interface XtdExternalDocumentService extends EntityService<XtdExternalDocument> {}
