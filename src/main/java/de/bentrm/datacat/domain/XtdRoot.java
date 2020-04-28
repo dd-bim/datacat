6c016d409d576dfc0a1ef5166724dc244ce28b4c
Benjamin Thurm <bentrm@posteo.de>
2020-04-28 09:14:23 +0200

    feat: Add composes relationships to XtdRoot

--- src/main/java/de/bentrm/datacat/domain/XtdRoot.java
+++ src/main/java/de/bentrm/datacat/domain/XtdRoot.java
@@ -12,141 +12,155 @@ import java.util.TreeSet;
 
 @NodeEntity(label = XtdRoot.LABEL)
 @PropertyQueryHint({
-		"(root)<-[:IS_NAME_OF|IS_DESCRIPTION_OF|COMMENTS*0..1]-()",
-		"(root)-[:ASSOCIATES]-()<-[:IS_NAME_OF|IS_DESCRIPTION_OF|COMMENTS*0..1]-()",
-		"(root)-[:GROUPS]-()<-[:IS_NAME_OF|IS_DESCRIPTION_OF|COMMENTS*0..1]-()",
-		"(root)-[:SPECIALIZES]-()<-[:IS_NAME_OF|IS_DESCRIPTION_OF|COMMENTS*0..1]-()",
-		"(root)-[:ACTS_UPON]-()<-[:IS_NAME_OF|IS_DESCRIPTION_OF|COMMENTS*0..1]-()"
+        "(root)<-[:IS_NAME_OF|IS_DESCRIPTION_OF|COMMENTS*0..1]-()",
+        "(root)-[:ASSOCIATES]-()<-[:IS_NAME_OF|IS_DESCRIPTION_OF|COMMENTS*0..1]-()",
+        "(root)-[:GROUPS]-()<-[:IS_NAME_OF|IS_DESCRIPTION_OF|COMMENTS*0..1]-()",
+        "(root)-[:SPECIALIZES]-()<-[:IS_NAME_OF|IS_DESCRIPTION_OF|COMMENTS*0..1]-()",
+        "(root)-[:ACTS_UPON]-()<-[:IS_NAME_OF|IS_DESCRIPTION_OF|COMMENTS*0..1]-()"
 })
 public abstract class XtdRoot extends XtdEntity implements Commented {
 
-	public static final String TITLE = "Root";
-	public static final String TITLE_PLURAL = "Roots";
-	public static final String LABEL = PREFIX + TITLE;
+    public static final String TITLE = "Root";
+    public static final String TITLE_PLURAL = "Roots";
+    public static final String LABEL = PREFIX + TITLE;
 
-	private String versionId;
+    private String versionId;
 
-	private String versionDate;
+    private String versionDate;
 
-	@Relationship(type = XtdDescription.RELATIONSHIP_TYPE, direction = Relationship.INCOMING)
-	private SortedSet<XtdDescription> descriptions = new TreeSet<>();
+    @Relationship(type = XtdDescription.RELATIONSHIP_TYPE, direction = Relationship.INCOMING)
+    private SortedSet<XtdDescription> descriptions = new TreeSet<>();
 
-	@Relationship(type = Comment.RELATIONSHIP_TYPE, direction = Relationship.INCOMING)
-	private SortedSet<Comment> comments = new TreeSet<>();
+    @Relationship(type = Comment.RELATIONSHIP_TYPE, direction = Relationship.INCOMING)
+    private SortedSet<Comment> comments = new TreeSet<>();
 
-	@Relationship(type = XtdRelCollects.RELATIONSHIP_TYPE)
-	private Set<XtdRelCollects> collects = new HashSet<>();
+    @Relationship(type = XtdRelCollects.RELATIONSHIP_TYPE)
+    private Set<XtdRelCollects> collects = new HashSet<>();
 
-	@Relationship(type = XtdRelCollects.RELATIONSHIP_TYPE, direction = Relationship.INCOMING)
-	private Set<XtdRelCollects> collectedIn = new HashSet<>();
+    @Relationship(type = XtdRelCollects.RELATIONSHIP_TYPE, direction = Relationship.INCOMING)
+    private Set<XtdRelCollects> collectedIn = new HashSet<>();
 
-	@Relationship(type = XtdRelDocuments.RELATIONSHIP_TYPE, direction = Relationship.INCOMING)
-	private final Set<XtdRelDocuments> documentedBy = new HashSet<>();
+    @Relationship(type = XtdRelDocuments.RELATIONSHIP_TYPE, direction = Relationship.INCOMING)
+    private final Set<XtdRelDocuments> documentedBy = new HashSet<>();
 
-	@Relationship(type = XtdRelAssociates.RELATIONSHIP_TYPE)
-	private final Set<XtdRelAssociates> associates = new HashSet<>();
+    @Relationship(type = XtdRelAssociates.RELATIONSHIP_TYPE)
+    private final Set<XtdRelAssociates> associates = new HashSet<>();
 
-	@Relationship(type = XtdRelAssociates.RELATIONSHIP_TYPE, direction = Relationship.INCOMING)
-	private final Set<XtdRelAssociates> associatedBy = new HashSet<>();
+    @Relationship(type = XtdRelAssociates.RELATIONSHIP_TYPE, direction = Relationship.INCOMING)
+    private final Set<XtdRelAssociates> associatedBy = new HashSet<>();
 
-	@Relationship(type = XtdRelGroups.RELATIONSHIP_TYPE)
-	private final Set<XtdRelGroups> groups = new HashSet<>();
+    @Relationship(type = XtdRelGroups.RELATIONSHIP_TYPE)
+    private final Set<XtdRelGroups> groups = new HashSet<>();
 
-	@Relationship(type = XtdRelGroups.RELATIONSHIP_TYPE, direction = Relationship.INCOMING)
-	private final Set<XtdRelGroups> groupedBy = new HashSet<>();
+    @Relationship(type = XtdRelGroups.RELATIONSHIP_TYPE, direction = Relationship.INCOMING)
+    private final Set<XtdRelGroups> groupedBy = new HashSet<>();
 
-	@Relationship(type = XtdRelSpecializes.RELATIONSHIP_TYPE)
-	private final Set<XtdRelSpecializes> specializes = new HashSet<>();
+    @Relationship(type = XtdRelSpecializes.RELATIONSHIP_TYPE)
+    private final Set<XtdRelSpecializes> specializes = new HashSet<>();
 
-	@Relationship(type = XtdRelSpecializes.RELATIONSHIP_TYPE, direction = Relationship.INCOMING)
-	private final Set<XtdRelSpecializes> specializedBy = new HashSet<>();
+    @Relationship(type = XtdRelSpecializes.RELATIONSHIP_TYPE, direction = Relationship.INCOMING)
+    private final Set<XtdRelSpecializes> specializedBy = new HashSet<>();
 
-	public String getVersionId() {
-		return versionId;
-	}
+    @Relationship(type = XtdRelComposes.RELATIONSHIP_TYPE)
+    private final Set<XtdRelSpecializes> composes = new HashSet<>();
 
-	public void setVersionId(String versionId) {
-		this.versionId = versionId;
-	}
+    @Relationship(type = XtdRelSpecializes.RELATIONSHIP_TYPE, direction = Relationship.INCOMING)
+    private final Set<XtdRelSpecializes> composedBy = new HashSet<>();
 
-	public String getVersionDate() {
-		return versionDate;
-	}
+    public String getVersionId() {
+        return versionId;
+    }
 
-	public void setVersionDate(String versionDate) {
-		this.versionDate = versionDate;
-	}
+    public void setVersionId(String versionId) {
+        this.versionId = versionId;
+    }
 
-	public SortedSet<XtdDescription> getDescriptions() {
-		return this.descriptions;
-	}
+    public String getVersionDate() {
+        return versionDate;
+    }
 
-	public void addDescription(XtdDescription newDescription) {
-		this.descriptions.add(newDescription);
-	}
+    public void setVersionDate(String versionDate) {
+        this.versionDate = versionDate;
+    }
 
-	public boolean removeDescription(XtdDescription description) {
-		return this.descriptions.remove(description);
-	}
+    public SortedSet<XtdDescription> getDescriptions() {
+        return this.descriptions;
+    }
 
-	public SortedSet<Comment> getComments() {
-		return comments;
-	}
+    public void addDescription(XtdDescription newDescription) {
+        this.descriptions.add(newDescription);
+    }
 
-	public void setComments(SortedSet<Comment> comments) {
-		this.comments = comments;
-	}
+    public boolean removeDescription(XtdDescription description) {
+        return this.descriptions.remove(description);
+    }
 
-	public Set<XtdRelCollects> getCollects() {
-		return collects;
-	}
+    public SortedSet<Comment> getComments() {
+        return comments;
+    }
 
-	public void setCollects(Set<XtdRelCollects> collects) {
-		this.collects = collects;
-	}
+    public void setComments(SortedSet<Comment> comments) {
+        this.comments = comments;
+    }
 
-	public Set<XtdRelCollects> getCollectedIn() {
-		return collectedIn;
-	}
+    public Set<XtdRelCollects> getCollects() {
+        return collects;
+    }
 
-	public void setCollectedIn(Set<XtdRelCollects> collectedIn) {
-		this.collectedIn = collectedIn;
-	}
+    public void setCollects(Set<XtdRelCollects> collects) {
+        this.collects = collects;
+    }
 
-	public Set<XtdRelDocuments> getDocumentedBy() {
-		return documentedBy;
-	}
+    public Set<XtdRelCollects> getCollectedIn() {
+        return collectedIn;
+    }
 
-	public Set<XtdRelAssociates> getAssociates() {
-		return associates;
-	}
+    public void setCollectedIn(Set<XtdRelCollects> collectedIn) {
+        this.collectedIn = collectedIn;
+    }
 
-	public Set<XtdRelAssociates> getAssociatedBy() {
-		return associatedBy;
-	}
+    public Set<XtdRelDocuments> getDocumentedBy() {
+        return documentedBy;
+    }
 
-	public Set<XtdRelGroups> getGroups() {
-		return groups;
-	}
+    public Set<XtdRelAssociates> getAssociates() {
+        return associates;
+    }
 
-	public Set<XtdRelGroups> getGroupedBy() {
-		return groupedBy;
-	}
+    public Set<XtdRelAssociates> getAssociatedBy() {
+        return associatedBy;
+    }
 
-	public Set<XtdRelSpecializes> getSpecializes() {
-		return specializes;
-	}
+    public Set<XtdRelGroups> getGroups() {
+        return groups;
+    }
 
-	public Set<XtdRelSpecializes> getSpecializedBy() {
-		return specializedBy;
-	}
+    public Set<XtdRelGroups> getGroupedBy() {
+        return groupedBy;
+    }
 
-	@Override
-	public String toString() {
-		return new ToStringBuilder(this)
-				.appendSuper(super.toString())
-				.append("versionId", versionId)
-				.append("versionDate", versionDate)
-				.toString();
-	}
+    public Set<XtdRelSpecializes> getSpecializes() {
+        return specializes;
+    }
+
+    public Set<XtdRelSpecializes> getSpecializedBy() {
+        return specializedBy;
+    }
+
+    public Set<XtdRelSpecializes> getComposes() {
+        return composes;
+    }
+
+    public Set<XtdRelSpecializes> getComposedBy() {
+        return composedBy;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .appendSuper(super.toString())
+                .append("versionId", versionId)
+                .append("versionDate", versionDate)
+                .toString();
+    }
 }
