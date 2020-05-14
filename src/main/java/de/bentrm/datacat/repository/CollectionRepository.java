27a8435ecc794acf871a98ef7042df4ef90ef9d0
Benjamin Thurm <bentrm@posteo.de>
2020-05-14 15:17:22 +0200

    feat: Add collection repository interface

---
+++ src/main/java/de/bentrm/datacat/repository/CollectionRepository.java
@@ -0,0 +1,6 @@
+package de.bentrm.datacat.repository;
+
+import de.bentrm.datacat.domain.collection.XtdCollection;
+
+public interface CollectionRepository extends GraphEntityRepository<XtdCollection, String> {
+}
