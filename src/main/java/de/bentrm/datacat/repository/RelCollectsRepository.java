4bee496c287f3cad24c9a6bf4c6f09099a115ad5
Benjamin Thurm <bentrm@posteo.de>
2020-05-14 15:17:02 +0200

    fix: Extend custom repository base class

--- src/main/java/de/bentrm/datacat/repository/RelCollectsRepository.java
+++ src/main/java/de/bentrm/datacat/repository/RelCollectsRepository.java
@@ -1,8 +1,7 @@
 package de.bentrm.datacat.repository;
 
 import de.bentrm.datacat.domain.relationship.XtdRelCollects;
-import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RelCollectsRepository extends Neo4jRepository<XtdRelCollects, String> {}
+public interface RelCollectsRepository extends GraphEntityRepository<XtdRelCollects, String> {}
