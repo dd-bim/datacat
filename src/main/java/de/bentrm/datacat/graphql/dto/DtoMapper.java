419816a1b70a5ae5617a84ef010df9e25ffb6950
Benjamin Thurm <bentrm@posteo.de>
2020-06-10 08:31:50 +0200

    feat: Make mapper available as a static instance on interface

--- src/main/java/de/bentrm/datacat/graphql/dto/DtoMapper.java
+++ src/main/java/de/bentrm/datacat/graphql/dto/DtoMapper.java
@@ -2,9 +2,11 @@ package de.bentrm.datacat.graphql.dto;
 
 import de.bentrm.datacat.service.Specification;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface DtoMapper {
+    DtoMapper INSTANCE = Mappers.getMapper(DtoMapper.class);
     Specification toSpecification(SearchInput input);
     Specification toSpecification(FilterInput input);
 }
