6c366a3065ab6ed40294a576baa2ff7ff640eda9
Benjamin Thurm <bentrm@posteo.de>
2020-06-25 14:10:03 +0200

    chore: Remove todo

--- src/main/java/de/bentrm/datacat/Application.java
+++ src/main/java/de/bentrm/datacat/Application.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-    // CALL db.index.fulltext.createNodeIndex("namesAndDescriptions",["XtdName", "XtdDescription"],["value"])
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
