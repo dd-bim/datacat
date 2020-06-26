d662d1cc1aa043535e67a26583114f8cc045bf7d
Benjamin Thurm <bentrm@posteo.de>
2020-06-26 10:31:54 +0200

    fix: Add missing property hint annotation

--- src/main/java/de/bentrm/datacat/domain/Entity.java
+++ src/main/java/de/bentrm/datacat/domain/Entity.java
@@ -13,6 +13,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.Instant;
 
+@PropertyQueryHint({})
 public abstract class Entity {
 
 	public static final String PREFIX = "Xtd";
