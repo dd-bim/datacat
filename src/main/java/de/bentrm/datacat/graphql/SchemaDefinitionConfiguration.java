3760ac5260756959874f3639b9029d187b1652e1
Benjamin Thurm <bentrm@posteo.de>
2020-07-03 13:36:43 +0200

    feat: Separate admin queries and mutations from domain graphql schema

--- src/main/java/de/bentrm/datacat/graphql/SchemaDefinitionConfiguration.java
+++ src/main/java/de/bentrm/datacat/graphql/SchemaDefinitionConfiguration.java
@@ -1,27 +1,35 @@
 package de.bentrm.datacat.graphql;
 
 import de.bentrm.datacat.graphql.fetcher.*;
+import graphql.schema.DataFetcher;
 import graphql.schema.GraphQLSchema;
 import graphql.schema.idl.*;
+import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ResourceLoaderAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 
-import java.io.File;
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
+import java.util.Map;
 import java.util.function.UnaryOperator;
 
+@Slf4j
 @Configuration
 public class SchemaDefinitionConfiguration implements ResourceLoaderAware {
 
     private final static String[] schemaFiles = {
-            "classpath:xtd.graphqls",
-            "classpath:query.graphqls",
-            "classpath:mutation.graphqls"
+            "classpath:graphql/xtd.graphqls",
+            "classpath:graphql/auth.graphqls",
+            "classpath:graphql/admin.graphqls",
+            "classpath:graphql/query.graphqls",
+            "classpath:graphql/mutation.graphqls"
     };
 
     private ResourceLoader resourceLoader;
@@ -68,63 +76,69 @@ public class SchemaDefinitionConfiguration implements ResourceLoaderAware {
     @Autowired
     private RelActsUponDataFetcherProvider relActsUponProvider;
 
+    final SchemaParser schemaParser = new SchemaParser();
+    final SchemaGenerator schemaGenerator = new SchemaGenerator();
+    final TypeDefinitionRegistry typeRegistry = new TypeDefinitionRegistry();
+    final RuntimeWiring.Builder builder = RuntimeWiring.newRuntimeWiring();
+
+    final UnaryOperator<TypeRuntimeWiring.Builder> rootFetchers = typeWiring -> {
+        rootDataFetcherProviders.forEach(provider -> {
+            final Map<String, DataFetcher> fetchers = provider.getRootDataFetchers();
+            typeWiring.dataFetchers(fetchers);
+        });
+        return typeWiring;
+    };
+    final UnaryOperator<TypeRuntimeWiring.Builder> objectFetchers = typeWiring -> {
+        rootFetchers.apply(typeWiring);
+        objectDataFetcherProviders.forEach(provider -> {
+            final Map<String, DataFetcher> fetchers = provider.getObjectDataFetchers();
+            typeWiring.dataFetchers(fetchers);
+        });
+        return typeWiring;
+    };
+
     @Bean
     GraphQLSchema schema() throws IOException {
-        final SchemaParser schemaParser = new SchemaParser();
-        final SchemaGenerator schemaGenerator = new SchemaGenerator();
-        final TypeDefinitionRegistry typeRegistry = new TypeDefinitionRegistry();
-        final RuntimeWiring.Builder builder = RuntimeWiring.newRuntimeWiring();
-        final UnaryOperator<TypeRuntimeWiring.Builder> rootDataFetchers = typeWiring -> {
-            rootDataFetcherProviders.forEach(provider -> typeWiring.dataFetchers(provider.getRootDataFetchers()));
-            return typeWiring;
-        };
-        final UnaryOperator<TypeRuntimeWiring.Builder> objectDataFetchers = typeWiring -> {
-            rootDataFetchers.apply(typeWiring);
-            objectDataFetcherProviders.forEach(provider -> typeWiring.dataFetchers(provider.getObjectDataFetchers()));
-            return typeWiring;
-        };
-
-        for (String location : schemaFiles) {
-            final File file = getFile(location);
-            final TypeDefinitionRegistry subRegistry = schemaParser.parse(file);
-            typeRegistry.merge(subRegistry);
-        }
-
+        parseSchema();
         typeResolvers.mapTypeResolvers(builder);
+        RuntimeWiring wiring = getRuntimeWiring();
+        return schemaGenerator.makeExecutableSchema(typeRegistry, wiring);
+    }
 
-        RuntimeWiring wiring = builder
+    private RuntimeWiring getRuntimeWiring() {
+        return builder
                 .type("Translation", typeWiring -> typeWiring.dataFetcher("language", baseDataFetcherProvider.languageByLanguageRepresentation()))
                 .type("XtdExternalDocument", typeWiring -> typeWiring
                         .dataFetchers(externalDocumentDataFetcherProvider.getDataFetchers()))
-                .type("XtdActivity", objectDataFetchers)
-                .type("XtdActor", objectDataFetchers)
-                .type("XtdClassification", objectDataFetchers)
-                .type("XtdSubject", objectDataFetchers)
-                .type("XtdUnit", objectDataFetchers)
-                .type("XtdProperty", objectDataFetchers)
-                .type("XtdValue", objectDataFetchers)
-                .type("XtdMeasureWithUnit", typeWiring -> objectDataFetchers
+                .type("XtdActivity", objectFetchers)
+                .type("XtdActor", objectFetchers)
+                .type("XtdClassification", objectFetchers)
+                .type("XtdSubject", objectFetchers)
+                .type("XtdUnit", objectFetchers)
+                .type("XtdProperty", objectFetchers)
+                .type("XtdValue", objectFetchers)
+                .type("XtdMeasureWithUnit", typeWiring -> objectFetchers
                         .apply(typeWiring)
                         .dataFetchers(measureWithUnitDataFetcherProvider.getPropertyDataFetchers()))
-                .type("XtdBag", rootDataFetchers)
-                .type("XtdNest", rootDataFetchers)
-                .type("XtdRelAssignsCollections", rootDataFetchers)
-                .type("XtdRelAssignsPropertyWithValues", rootDataFetchers)
-                .type("XtdRelDocuments", rootDataFetchers)
-                .type("XtdRelCollects", rootDataFetchers)
-                .type("XtdRelAssociates", typeWiring -> rootDataFetchers
+                .type("XtdBag", rootFetchers)
+                .type("XtdNest", rootFetchers)
+                .type("XtdRelAssignsCollections", rootFetchers)
+                .type("XtdRelAssignsPropertyWithValues", rootFetchers)
+                .type("XtdRelDocuments", rootFetchers)
+                .type("XtdRelCollects", rootFetchers)
+                .type("XtdRelAssociates", typeWiring -> rootFetchers
                         .apply(typeWiring)
                         .dataFetchers(relAssociatesProvider.getRelAssociatesDataFetchers()))
-                .type("XtdRelGroups", typeWiring -> rootDataFetchers
+                .type("XtdRelGroups", typeWiring -> rootFetchers
                         .apply(typeWiring)
                         .dataFetchers(relGroupsProvider.getRelGroupsDataFetchers()))
-                .type("XtdRelSpecializes", typeWiring -> rootDataFetchers
+                .type("XtdRelSpecializes", typeWiring -> rootFetchers
                         .apply(typeWiring)
                         .dataFetchers(relSpecializesProvider.getRelSpecializesDataFetchers()))
-                .type("XtdRelComposes", typeWiring -> rootDataFetchers
+                .type("XtdRelComposes", typeWiring -> rootFetchers
                         .apply(typeWiring)
                         .dataFetchers(relComposesProvider.getRelComposesDataFetchers()))
-                .type("XtdRelActsUpon", typeWiring -> rootDataFetchers
+                .type("XtdRelActsUpon", typeWiring -> rootFetchers
                         .apply(typeWiring)
                         .dataFetchers(relActsUponProvider.getRelActsUponDataFetchers()))
                 .type("Query", typeWiring -> {
@@ -136,7 +150,15 @@ public class SchemaDefinitionConfiguration implements ResourceLoaderAware {
                     return typeWiring;
                 })
                 .build();
-        return schemaGenerator.makeExecutableSchema(typeRegistry, wiring);
+    }
+
+    private void parseSchema() throws IOException {
+        for (String location : schemaFiles) {
+            final BufferedInputStream inputStream = getSchemaFileImputStream(location);
+            log.debug("Parsing GraphQL schema file {}", location);
+            final TypeDefinitionRegistry subRegistry = schemaParser.parse(inputStream);
+            typeRegistry.merge(subRegistry);
+        }
     }
 
     @Override
@@ -144,7 +166,10 @@ public class SchemaDefinitionConfiguration implements ResourceLoaderAware {
         this.resourceLoader = resourceLoader;
     }
 
-    public File getFile(String location) throws IOException {
-        return resourceLoader.getResource(location).getFile();
+    public BufferedInputStream getSchemaFileImputStream(String location) throws IOException {
+        final Resource resource = resourceLoader.getResource(location);
+        log.debug("Classpath resource {} available: {}", location, resource.exists());
+        final InputStream inputStream = resource.getInputStream();
+        return new BufferedInputStream(inputStream);
     }
 }
