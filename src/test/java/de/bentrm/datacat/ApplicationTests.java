3b1c6219e5104109269ee6c5619b68b8bd3d4e4b
Benjamin Thurm <bentrm@posteo.de>
2020-07-14 09:36:33 +0200

    fix: test configuration

--- src/test/java/de/bentrm/datacat/ApplicationTests.java
+++ src/test/java/de/bentrm/datacat/ApplicationTests.java
@@ -2,8 +2,10 @@ package de.bentrm.datacat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class ApplicationTests {
 
 	@Test
