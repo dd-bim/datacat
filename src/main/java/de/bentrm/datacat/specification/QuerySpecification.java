0298be8bd78772caafe92070586481a4693d8f07
Benjamin Thurm <bentrm@posteo.de>
2020-08-11 11:00:13 +0200

    feat: Implement basic specification query logic

---
+++ src/main/java/de/bentrm/datacat/specification/QuerySpecification.java
@@ -0,0 +1,43 @@
+package de.bentrm.datacat.specification;
+
+import lombok.Getter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+import org.neo4j.ogm.cypher.Filters;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Getter
+@ToString
+public abstract class QuerySpecification {
+
+    private final Filters filters;
+    private final Sort.Direction sortDirection;
+    private final List<String> sortBy;
+    private final Integer pageNumber;
+    private final Integer pageSize;
+
+    protected QuerySpecification(final Filters filters, final Sort.Direction sortDirection, final List<String> sortBy, final Integer pageNumber, final Integer pageSize) {
+        this.filters = filters;
+        this.sortDirection = sortDirection != null ? sortDirection : Sort.Direction.ASC;
+        this.sortBy = sortBy;
+        this.pageNumber = pageNumber != null ? pageNumber : 0;
+        this.pageSize = pageSize;
+    }
+
+    public Optional<Pageable> getPageable() {
+        if (pageSize != null) {
+            if (sortBy != null) {
+                final Sort sort = Sort.by(sortDirection, sortBy.toArray(new String[0]));
+                return Optional.of(PageRequest.of(pageNumber, pageSize, sort));
+            }
+            return Optional.of(PageRequest.of(pageNumber, pageSize));
+        }
+        return Optional.empty();
+    }
+}
