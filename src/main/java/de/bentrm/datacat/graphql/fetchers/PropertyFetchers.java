3a4387e1b6d08c2ca71240a0f2448b3ac04eba1a
Benjamin Thurm <bentrm@posteo.de>
2020-11-11 19:08:58 +0100

    fix: nullable relationship collection

--- src/main/java/de/bentrm/datacat/graphql/fetchers/PropertyFetchers.java
+++ src/main/java/de/bentrm/datacat/graphql/fetchers/PropertyFetchers.java
@@ -1,14 +1,22 @@
 package de.bentrm.datacat.graphql.fetchers;
 
 import de.bentrm.datacat.catalog.domain.XtdProperty;
+import de.bentrm.datacat.catalog.service.AssignsPropertiesService;
 import de.bentrm.datacat.catalog.service.PropertyService;
+import de.bentrm.datacat.graphql.fetcher.AssignsPropertiesFetcher;
+import graphql.schema.DataFetcher;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 @Component
 public class PropertyFetchers extends AbstractObjectFetchers<XtdProperty, PropertyService> {
 
-    public PropertyFetchers(PropertyService entityService) {
+    private final AssignsPropertiesFetcher assignsPropertiesFetcher;
+
+    public PropertyFetchers(PropertyService entityService, AssignsPropertiesService assignsPropertiesService) {
         super(entityService);
+        this.assignsPropertiesFetcher = new AssignsPropertiesFetcher(assignsPropertiesService);
     }
 
     @Override
@@ -29,5 +37,12 @@ public class PropertyFetchers extends AbstractObjectFetchers<XtdProperty, Proper
     @Override
     public String getMutationNameSuffix() {
         return "Property";
+    }
+
+    @Override
+    public Map<String, DataFetcher> getAttributeFetchers() {
+        final Map<String, DataFetcher> fetchers = super.getAttributeFetchers();
+        fetchers.put("assignedTo", assignsPropertiesFetcher.assignedTo());
+        return fetchers;
     }
 }
