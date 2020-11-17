723c0772d26bc9e8f64d503db6866ed5339fea18
Benjamin Thurm <bentrm@posteo.de>
2020-11-17 13:28:46 +0100

    fix: Add missing relationship repositories

---
+++ src/main/java/de/bentrm/datacat/catalog/repository/AssignsValuesRelationshipRepository.java
@@ -0,0 +1,7 @@
+package de.bentrm.datacat.catalog.repository;
+
+import de.bentrm.datacat.base.repository.GraphEntityRepository;
+import de.bentrm.datacat.catalog.domain.XtdRelAssignsValues;
+
+public interface AssignsValuesRelationshipRepository extends GraphEntityRepository<XtdRelAssignsValues> {
+}
