065dfcd4eccc42ec293118b928a4662bbc3b0282
Benjamin Thurm <bentrm@posteo.de>
2020-12-01 11:10:21 +0100

    fix: Remove unnecessary datastore configuration overrides

--- src/main/java/de/bentrm/datacat/DataStoreConfiguration.java
+++ src/main/java/de/bentrm/datacat/DataStoreConfiguration.java
@@ -1,65 +1,29 @@
 package de.bentrm.datacat;
 
-import de.bentrm.datacat.base.repository.GraphEntityRepositoryFactoryBean;
-import org.neo4j.ogm.session.SessionFactory;
-import org.neo4j.ogm.session.event.EventListener;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.boot.autoconfigure.data.neo4j.Neo4jProperties;
-import org.springframework.boot.autoconfigure.transaction.TransactionManagerCustomizers;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
-import org.springframework.data.neo4j.transaction.Neo4jTransactionManager;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.util.Optional;
 
 @Configuration
-@EnableConfigurationProperties({Neo4jProperties.class})
 @EnableNeo4jRepositories(
         basePackages = {
                 "de.bentrm.datacat.base.repository",
                 "de.bentrm.datacat.auth.repository",
                 "de.bentrm.datacat.catalog.repository"
-        },
-        repositoryFactoryBeanClass = GraphEntityRepositoryFactoryBean.class
+        }
 )
 @EnableTransactionManagement
 public class DataStoreConfiguration {
 
     @Bean
-    public org.neo4j.ogm.config.Configuration configuration(Neo4jProperties properties) {
-        return properties.createConfiguration();
-    }
-
-    @Bean
-    SessionFactory sessionFactory(org.neo4j.ogm.config.Configuration configuration, BeanFactory beanFactory, ObjectProvider<EventListener> eventListeners) {
-        SessionFactory sessionFactory = new SessionFactory(
-                configuration,
-                "de.bentrm.datacat.base.domain",
-                "de.bentrm.datacat.auth.domain",
-                "de.bentrm.datacat.catalog.domain"
-        );
-        eventListeners.orderedStream().forEach(sessionFactory::register);
-        return sessionFactory;
-    }
-
-    @Bean
-    public Neo4jTransactionManager transactionManager(SessionFactory sessionFactory, ObjectProvider<TransactionManagerCustomizers> transactionManagerCustomizers) {
-        Neo4jTransactionManager transactionManager = new Neo4jTransactionManager(sessionFactory);
-        transactionManagerCustomizers.ifAvailable((customizers) -> {
-            customizers.customize(transactionManager);
-        });
-        return transactionManager;
-    }
-
-    @Bean
     public AuditorAware<String> auditorAware() {
-        return () -> Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+        return () -> Optional.ofNullable(SecurityContextHolder.getContext()
+                .getAuthentication())
                 .map(auth -> (String) auth.getPrincipal())
                 .or(() -> Optional.of("SYSTEM"));
     }
